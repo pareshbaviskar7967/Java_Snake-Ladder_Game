28e114b76d48ed4037d12b91778b2fb1df72011f
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-05-25 17:27:50 +0530

    UC-3 Check options NoPlay,Snake,Ladder

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -1,12 +1,29 @@
+import java.util.Random;
+
 public class Snake_Ladder {
+
+    public static final int No_Play = 0;
+    public static final int Ladder = 1;
+    public static final int Snake = 2;
 
     public static void main(String[] args) {
 
         int playerposition = 0;
         System.out.println("The start position of player is : " + playerposition);
-
-        int diceroll = (int) (Math.random() * 6) + 1;
-        playerposition += diceroll;
+        Random ranval = new Random();
+        int diceroll = ranval.nextInt(6) + 1;
+        int check_play = ranval.nextInt(3);
+        switch (check_play) {
+            case No_Play:
+                break;
+            case Ladder:
+                playerposition += diceroll;
+                break;
+            case Snake:
+                playerposition -= diceroll;
+                break;
+        }
         System.out.println("After the roll dice....The position of player is : " + playerposition);
+
     }
 }
