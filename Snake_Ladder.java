8c75735f37ab25165c617f621106b013a075080c
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-05-25 16:57:13 +0530

    UC-2 Player position after roll dice.

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -1,7 +1,12 @@
 public class Snake_Ladder {
 
     public static void main(String[] args) {
+
         int playerposition = 0;
-        System.out.println("The position of player is : " + playerposition);
+        System.out.println("The start position of player is : " + playerposition);
+
+        int diceroll = (int) (Math.random() * 6) + 1;
+        playerposition += diceroll;
+        System.out.println("After the roll dice....The position of player is : " + playerposition);
     }
 }
