0085ffdb970fa91bcda4521e7f18859bb374bf51
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-05-25 16:43:31 +0530

    UC-1 Player at start position 0

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -1,6 +1,7 @@
 public class Snake_Ladder {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Snake and ladder game.....!");
+        int playerposition = 0;
+        System.out.println("The position of player is : " + playerposition);
     }
 }
